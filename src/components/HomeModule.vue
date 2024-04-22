e18f5701e8565c5dfbf91e80aded70551f726e66
luna <qi.jiandong.cn@gmail.com>
2024-04-22 16:16:34 +0800

    chore: normalize filename

--- src/components/HomeModule.vue
+++ src/components/HomeModule.vue
@@ -2,7 +2,7 @@
   <el-container class="home-container">
     <el-header>
       <div>
-        <img src="../assets/heima.png" alt="logo">
+        <img src="../assets/head.png" alt="logo">
         <span>电商后台管理系统</span>
       </div>
       <el-button type="info" @click="logout">退出</el-button>
